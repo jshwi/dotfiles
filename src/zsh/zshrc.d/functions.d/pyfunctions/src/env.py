29627c0f1d129eccf687f9500393957f3c44e077
jshwi <stephen@jshwisolutions.com>
2020-11-29 00:07:50 +1100

    [feat] Adds gnupg and env var

--- src/zsh/zshrc.d/functions.d/pyfunctions/src/env.py
+++ src/zsh/zshrc.d/functions.d/pyfunctions/src/env.py
@@ -3,8 +3,10 @@ pyfunctions.env
 ===============
 """
 import datetime
+import os
 import pathlib
 
 HOME = str(pathlib.Path.home())
 DATE = datetime.date.today().strftime("%Y/%m/%d")
 TIME = datetime.datetime.now().strftime("%H:%M:%S")
+GNUPG_PASSPHRASE = os.environ.get("GNUPG_PASSPHRASE", "")
